e55a00c0bc84aa324f00125940fdcc7b0f075fa1
tackchen <tackchen@tencent.com>
2022-04-05 15:19:06 +0800

    feat: 1

---
+++ js/stat.js
@@ -0,0 +1,9 @@
+// 统计代码
+(function(){
+    var id = "1281058979";
+    var script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("src", "https://v1.cnzz.com/z_stat.php?id="+id+"&web_id="+id);
+    window.document.body.appendChild(script);
+    window.__view_stat = function () { window.open("https://www.cnzz.com/stat/website.php?web_id="+id); }
+})();
