e227788ab61d5f96a523acfaf84e59f6859c314c
tackchen <tackchen@tencent.com>
2022-04-28 16:56:38 +0800

    fix: stat 禁用gitee pages

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,16 @@
 // 友盟站点统计代码
-(function(){
-    var id = "1281058979";
-    var script = document.createElement("script");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute("src", "https://v1.cnzz.com/z_stat.php?id="+id+"&web_id="+id);
-    window.document.body.appendChild(script);
-    window.__view_stat = function () { window.open("https://www.cnzz.com/stat/website.php?web_id="+id); }
+(function () {
+    if (location.host !== 'theajack.gitee.io') { // gitee page 不上报 可能会导致被禁用
+        var id = '1281058979';
+        var script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute(
+            'src',
+            'https://v1.cnzz.com/z_stat.php?id=' + id + '&web_id=' + id
+        );
+        window.document.body.appendChild(script);
+        window.__view_stat = function () {
+            window.open('https://www.cnzz.com/stat/website.php?web_id=' + id);
+        };
+    }
 })();
